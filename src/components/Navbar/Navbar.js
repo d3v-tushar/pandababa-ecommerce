1a1369c0b21560c30753d776fd2b358dc2c6f517
d3v-tushar <d3v.tushar@gmail.com>
2023-02-10 03:20:30 +0600

    Bug Fixing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,10 +4,11 @@ import { BiSearchAlt } from "react-icons/bi";
 import { CgProfile } from "react-icons/cg";
 import { HiOutlineShoppingBag } from "react-icons/hi";
 import { IoIosArrowDown } from "react-icons/io";
+import { BiCategory } from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <div className="bg-white py-5">
+    <div className="bg-white pt-5 pb-3">
       <nav className="w-full lg:w-4/5 grid grid-cols-5 justify-between mx-auto align-middle items-center">
         <div className="col-span-1">
           <img src={logo} alt="" />
@@ -15,12 +16,12 @@ const Navbar = () => {
         <div className="relative col-span-3">
           <label class="flex overflow-hidden">
             <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-              <span class="text-2xl fill-slate-300 text-gray-500">
+              <span class="text-2xl fill-slate-300 text-gray-500 px-2">
                 <BiSearchAlt />
               </span>
             </span>
             <input
-              class="placeholder:text-slate-400 block bg-white w-full border border-gray-400 rounded-full py-3 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              class="placeholder:text-slate-400 block bg-white w-full border border-gray-400 rounded-full py-3 pl-12 pr-3 shadow-sm sm:text-sm"
               placeholder="Search for anything..."
               type="text"
               name="search"
@@ -39,6 +40,22 @@ const Navbar = () => {
           </button>
         </div>
       </nav>
+
+      {/* {Navbar 2nd Row} */}
+      <div className="flex mt-6 justify-between w-full lg:w-4/5 mx-auto">
+        <div className="mx-2 bg-gray-100">
+            {/* <div><button className="pr-12"><div className="flex items-baseline"><p className="flex"><span className="text-2xl"><BiCategory/></span> Categories</p><p><IoIosArrowDown/></p></div></button></div> */}
+            <div className="w-full"><div className="flex items-baseline"><p className="flex"><span className="text-2xl"><BiCategory/></span> Categories</p><p><IoIosArrowDown/></p></div></div>
+        </div>
+        <div className="col-span-4 flex justify-evenly">
+            <button className="mx-2 px-3">Home <IoIosArrowDown /></button>
+            <button className="mx-2 px-3">Mega Menu <IoIosArrowDown /></button>
+            <button className="mx-2 px-3">Full Screen Menu <IoIosArrowDown /></button>
+            <button className="mx-2 px-3">Pages <IoIosArrowDown/></button>
+            <button className="mx-2 px-3">User Account <IoIosArrowDown /></button>
+            <button className="mx-2 px-3">Vendor Account <IoIosArrowDown /></button>
+        </div>
+      </div>
     </div>
   );
 };
