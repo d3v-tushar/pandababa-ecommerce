5380664d2800889e9e3b1ab1d81a51780825c123
d3v-tushar <108591841+d3v-tushar@users.noreply.github.com>
2023-02-24 00:36:39 +0000

    Small UI Bug Fixed

--- src/components/ProductsFromCategory/ProductsFromCategory.js
+++ src/components/ProductsFromCategory/ProductsFromCategory.js
@@ -7,21 +7,21 @@ const ProductsFromCategory = ({categoryName}) => {
     return (
         <section className='grid grid-cols-4 gap-x-6 my-6 rounded-sm'>
             <div className='col-span-1 text-left bg-white px-8 py-4'>
-                <h3 className='text-[20px] font-bold text-gray-700'>{categoryName}</h3>
-                <ul className='list-none py-2 mb-2 leading-6'>
-                    <li className='pt-2.5'><a className='hover:text-red-500 text-[14px] font-semibold ' href="/">Wireless Speaker</a></li>
-                    <li className='pt-2.5'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Tablet</a></li>
-                    <li className='pt-2.5'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Smartphone</a></li>
-                    <li className='pt-2.5'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Laptop</a></li>
-                    <li className='pt-2.5'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">iMac</a></li>
-                    <li className='pt-2.5'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Game Controller</a></li>
-                    <li className='pt-2.5'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Drone</a></li>
-                    <li className='pt-2.5'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Apple</a></li>
+                <h3 className='text-xl font-bold text-gray-700'>{categoryName}</h3>
+                <ul className='list-none py-2 mb-2 leading-5 lg:leading-6'>
+                    <li className='py-1'><a className='hover:text-red-500 text-[14px] font-semibold ' href="/">Wireless Speaker</a></li>
+                    <li className='py-1'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Tablet</a></li>
+                    <li className='py-1'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Smartphone</a></li>
+                    <li className='py-1'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Laptop</a></li>
+                    <li className='py-1'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">iMac</a></li>
+                    <li className='py-1'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Game Controller</a></li>
+                    <li className='py-1'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Drone</a></li>
+                    <li className='py-1'><a className='hover:text-red-500 text-[14px] font-semibold' href="/">Apple</a></li>
                 </ul>
                 <a className="text-sm font-semibold" href="/market-2#">
                 <p className="flex items-center text-left">
                   <span className='text-md  link link-underline link-underline-black text-black'>Browse All</span>
-                  <span className="mt-3 mb-3 mx-2">
+                  <span className="mt-2 mb-1 mx-2">
                     <AiOutlineArrowRight />
                   </span>
                 </p>
